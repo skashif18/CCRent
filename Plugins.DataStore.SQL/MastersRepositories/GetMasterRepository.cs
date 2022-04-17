8f5b2b87b29b1460f3435bd5eaab0b8d1045c6ab
kashif abbas <skashif18@outlook.com>
2022-04-17 16:23:41 +0300

    Repo added

---
+++ Plugins.DataStore.SQL/MastersRepositories/GetMasterRepository.cs
@@ -0,0 +1,47 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces.Masters;
+
+namespace Plugins.DataStore.SQL.MastersRepositories
+{
+    public class GetMasterRepository : IGetMasterRepository
+    {
+        private readonly CarRentContext db;
+        public GetMasterRepository(CarRentContext _db)
+        {
+            db = _db;
+        }
+
+        public IEnumerable<IdNameModel> GetCities()
+        => from item in db.SysCities
+           select new IdNameModel
+           {
+               Id = item.Id,
+               Name = item.NameEn
+           };
+
+        public IEnumerable<IdNameModel> GetGenders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IdNameModel> GetLanguages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IdNameModel> GetNationalities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IdNameModel> GetReligions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
