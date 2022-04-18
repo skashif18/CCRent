d391c2396153da3a555b81577b35b41136327c6d
sakunaa <sydkawish121@gmail.com>
2022-04-18 04:39:47 -0700

    Added Class Interface and Repo

---
+++ Plugins.DataStore.SQL/ServiceRepository/ClassRepository.cs
@@ -0,0 +1,57 @@
+﻿using CoreBusiness;
+using CoreBusiness.Master;
+using System;
+using System.Collections.Generic;
+using UseCases.DataStorePluginInterfaces.SrvTable.SrvMaster;
+
+namespace Plugins.DataStore.SQL.ServiceRepository
+{
+    public class ClassRepository : IClassRepository
+    {
+        private readonly CarRentContext db;
+        private readonly Response response;
+        public ClassRepository(CarRentContext _db)
+        {
+            db = _db;
+        }
+        public Response Create(SrvClass model)
+        {
+            try
+            {
+                db.Add(model);
+                db.SaveChanges();
+                response.IsSuccess = true;
+                response.Message = "Added Successfully";
+                response.Objects = model;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = "Error: " + ex.Message;
+            }
+
+            return response;
+        }
+
+        public Response Delete(int Id)
+        {
+            throw new System.NotImplementedException();
+        }   
+
+        public IEnumerable<SrvClass> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public SrvClass GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Response Update(SrvClass model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
---
+++ UseCases/DataStorePluginInterfaces/SrvTable/SrvMaster/IClassRepository.cs
@@ -0,0 +1,19 @@
+﻿using CoreBusiness;
+using CoreBusiness.Master;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCases.DataStorePluginInterfaces.SrvTable.SrvMaster
+{
+    public  interface IClassRepository
+    {
+        Response Create(SrvClass model);
+        Response Update(SrvClass model);
+        IEnumerable<SrvClass> GetAll();
+        SrvClass GetById(int id);
+        Response Delete(int Id);
+    }
+}
